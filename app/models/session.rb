ff06b6847dd30e2093b63df669d3d59e71554ff0
aude cateland <aude.cateland@gmail.com>
2022-11-30 00:01:46 +0100

    seed clean and scalable

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,6 +2,6 @@ class Session < ApplicationRecord
   belongs_to :user
   has_many :sequences
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, uniqueness: { scope: :user_id }
   validates :category, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,71 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p 'cleaning db.....'
 
-# users = ['aude', 'mat', 'pedro', 'nicolo']
+Track.destroy_all
+Sequence.destroy_all
+Session.destroy_all
+User.destroy_all
 
-# users.each do |user|
-#   user = User.create!(password: 'azerty', email: "#{user}@mail.fr")
-# end
+#users
+users = %w[aude mat pedro nicolo]
 
-aude = User.create!(password: 'azerty', email: 'aude@mail.fr')
-mat = User.create!(password: 'azerty', email: 'mat@mail.fr')
-pedro = User.create!(password: 'azerty', email: 'pedro@mail.fr')
-nicolo = User.create!(password: 'azerty', email: 'nicolo@mail.fr')
+#sessions
+sessions_seed = [{ name: "run", category: "sport" }, { name: "yoga", category: "sport" }, { name: "pasta", category: "cuisine" }, { name: "concentration", category: "travail" }, { name: "balade", category: "promenade" }]
 
-Run = Session.create(name: 'Run', category: 'Sport', user_id: 1)
-Yoga = Session.create(name: 'Yoga', category: 'Sport', user_id: 2)
-Pasta = Session.create(name: 'Pasta', category: 'Cuisine', user_id: 3)
-Travail = Session.create(name: 'Travail', category: 'Travail', user_id: 4)
-Balade = Session.create(name: 'Balade', category: 'Promenade', user_id: 4)
+# sequences
+seq_un = { name: 'un', duration: '5', playlist_source_id: 'a' }
+seq_deux = { name: 'deux', duration: '10', playlist_source_id: 'b', session_id: 2 }
+seq_trois = { name: 'trois', duration: '6', playlist_source_id: 'c', session_id: 3 }
+seq_quatre = { name: 'quatre', duration: '8', playlist_source_id: 'd', session_id: 4 }
 
-un = Sequence.create!(name: 'un', duration: '2', playlist_source_id: 'a', session_id: 1)
-deux = Sequence.create!(name: 'deux', duration: '4', playlist_source_id: 'b', session_id: 2)
-trois = Sequence.create!(name: 'trois', duration: '6', playlist_source_id: 'c', session_id: 3)
-quatre = Sequence.create!(name: 'quatre', duration: '8', playlist_source_id: 'd', session_id: 4)
+# tracks
+tracks_seed_a = [{ title: 'sur la route', artist: 'De palmas' }, { title: 'je roule', artist: 'Soprano' }, { title: 'Les lacs du Connemara', artist: 'Sardou Michou' }, { title: 'My heart will go on', artist: 'Celine' }, { title: 'La fièvre', artist: 'NTM' }]
 
-track1 = Track.create!(title:'sur la route', artist: 'De palmas', sequence_id: 1)
-track2 = Track.create!(title:'je roule', artist: 'Soprano', sequence_id: 2)
-track4 = Track.create!(title:'Les lacs du Connemara', artist: 'Sardou Michou', sequence_id: 3)
-track5 = Track.create!(title:'My heart will go on', artist: 'Celine', sequence_id: 4)
-track6 = Track.create!(title:'La fièvre', artist: 'NTM', sequence_id: 1)
+tracks_seed_b = [{ title:'toxic', artist: 'Britney baby' }, { title: 'cette année là', artist: 'Cloclo' }, { title:'Champs Elysee', artist: 'Joe Dassin' }, { title:'La boulette', artist: 'Diams' }, { title:'Stach Stach', artist: 'Anthologigi' }]
+
+users.each do |user|
+  user = User.create!(password: 'azerty', email: "#{user}@mail.fr")
+  new_session = user.sessions.create!(sessions_seed.sample)
+  p "created session: #{new_session}"
+  p "generating sequences"
+  [seq_un, seq_deux].each do |attributes|
+    new_sequence = new_session.sequences.create!(attributes)
+    p "created sequence #{new_sequence}"
+    new_sequence.tracks.create!(tracks_seed_a)
+    p "created sequence #{Track.last}"
+  end
+
+  [seq_trois, seq_quatre].each do |attributes|
+    new_sequence = new_session.sequences.create!(attributes)
+    p "created sequence #{new_sequence}"
+    new_sequence.tracks.create!(tracks_seed_b)
+    p "created sequence #{Track.all}"
+  end
+end
+puts "Seed finished!"
+
+
+# aude = User.create!(password: 'azerty', email: 'aude@mail.fr')
+# mat = User.create!(password: 'azerty', email: 'mat@mail.fr')
+# pedro = User.create!(password: 'azerty', email: 'pedro@mail.fr')
+# nicolo = User.create!(password: 'azerty', email: 'nicolo@mail.fr')
+
+
+# Run = Session.create(name: 'Run', category: 'Sport', user_id: 1)
+# Yoga = Session.create(name: 'Yoga', category: 'Sport', user_id: 2)
+# Pasta = Session.create(name: 'Pasta', category: 'Cuisine', user_id: 3)
+# Travail = Session.create(name: 'Travail', category: 'Travail', user_id: 4)
+# Balade = Session.create(name: 'Balade', category: 'Promenade', user_id: 4)
+
+# un = Sequence.create!(name: 'un', duration: '2', playlist_source_id: 'a', session_id: 1)
+# deux = Sequence.create!(name: 'deux', duration: '4', playlist_source_id: 'b', session_id: 2)
+# trois = Sequence.create!(name: 'trois', duration: '6', playlist_source_id: 'c', session_id: 3)
+# quatre = Sequence.create!(name: 'quatre', duration: '8', playlist_source_id: 'd', session_id: 4)
+
+# track1 = Track.create!(title:'sur la route', artist: 'De palmas', sequence_id: 1)
+# track2 = Track.create!(title:'je roule', artist: 'Soprano', sequence_id: 2)
+# track4 = Track.create!(title:'Les lacs du Connemara', artist: 'Sardou Michou', sequence_id: 3)
+# track5 = Track.create!(title:'My heart will go on', artist: 'Celine', sequence_id: 4)
+# track6 = Track.create!(title:'La fièvre', artist: 'NTM', sequence_id: 1)

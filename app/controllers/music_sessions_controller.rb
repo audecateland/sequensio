cd3d11c2ea8a0031d660638a6a5649a425078b33
mathieu chirac <astarte19dev@gmail.com>
2022-11-30 15:58:20 +0100

    CRUD

--- app/controllers/music_sessions_controller.rb
+++ app/controllers/music_sessions_controller.rb
@@ -1,25 +1,41 @@
 class MusicSessionsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
-
   def index
+    @sessions = Session.all
   end
 
   def show
+    @session = Session.find(params[:id])
   end
 
-  def duplicate
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(session_params)
+    @session.user = current_user
+    if @session.save
+      redirect_to session_path(@session)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+  end
+
+  def duplicate
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:name, :category, :description)
   end
 end

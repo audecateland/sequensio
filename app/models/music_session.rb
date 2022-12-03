5c61fbe961e94f790dec191874df78da1c69ad1e
nicolas monmerque <nicolas.monmerque@gmail.com>
2022-12-03 17:42:52 +0100

    Suite de la home page avec les accordeons

--- app/models/music_session.rb
+++ app/models/music_session.rb
@@ -4,4 +4,9 @@ class MusicSession < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
   validates :category, presence: true
+
+  def chiffre
+    number = 1
+    number = number + 1
+  end
 end

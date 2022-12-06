49177a42c0fc265a275ac2046de496c9fbbf6be3
aude cateland <aude.cateland@gmail.com>
2022-12-06 11:28:36 +0100

    destroy sequence done

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -32,16 +32,16 @@ class SequencesController < ApplicationController
     end
   end
 
+  def destroy
+    @sequence = Sequence.find(params[:id])
+    @sequence.destroy
+    redirect_to music_session_path, status: :see_other
+  end
+
   private
 
   def sequence_params
     params.require(:sequence).permit(:name, :duration,
     :playlist_source_name, :transition)
-  end
-
-  def destroy_sequence
-    @sequence = Sequence.find(params[:id])
-    @sequence.destroy
-    redirect_to music_sessions_path, status: :see_other
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :music_sessions, only: [:new, :create, :destroy, :index, :show, :duplicate, :edit, :update] do
-    resources :sequences, only: [:edit, :update, :new, :create, :shuffle, :destroy]
+    resources :sequences, only: [:edit, :update, :new, :create, :shuffle]
   end
-
+  resources :sequences, only: :destroy
   resources :tracks, only: [:update, :index]
 end

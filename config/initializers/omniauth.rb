0d77cee3d5169b3567c4a3441142f3bc8af64206
aude cateland <aude.cateland@gmail.com>
2022-12-07 12:41:04 +0100

    remove omniauth to allow spotify

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-require 'rspotify/oauth'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['RSPOTIFY_CLIENT_ID'], ENV['RSPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-end
-
-OmniAuth.config.allowed_request_methods = [:post, :get]

ce59634280e4bdf44c064933004227ad86401b16
mathieu chirac <astarte19dev@gmail.com>
2022-12-08 17:09:37 +0100

    shuffle_a_track repaired

--- app/models/music_session.rb
+++ app/models/music_session.rb
@@ -7,23 +7,6 @@ class MusicSession < ApplicationRecord
   validates :name, uniqueness: { scope: :user_id }
   validates :category, presence: true
 
-
-  ########
-  # def create_playlist
-  #   RSpotify::authenticate(ENV['RSPOTIFY_CLIENT_ID'], ENV['RSPOTIFY_CLIENT_SECRET'])
-  #   #me = RSpotify::User.new(request.env['omniauth.auth'])
-  #   me = RSpotify::User.find(self.user.uid)
-  #   # debugger
-  #   me.create_playlist!("coco")
-  #   tracks = []
-  #   self.sequences.each do |sequence|
-  #     tracks << sequence.tracks
-  #   end
-  #   playlist.add_tracks!(tracks)
-
-  # end
-  ########
-
   def play
     playlist = spotify_user.create_playlist!(music_session.name)
     music_session.sequences.each do |sequence|
@@ -34,10 +17,5 @@ class MusicSession < ApplicationRecord
   def total_tracks_duration
     tracks.sum(:duration)
   end
-
-
-  # def self.search
-  #   tag = MusicSession.find_by(:category)
-  # end
 
 end
--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -35,28 +35,43 @@ class Sequence < ApplicationRecord
   end
 
   def shuffle_a_track(track_id)
-    timecount = 0
+    # timecount = 0
     # tant que il ne dépasse pas le temps indiqué par le user
     # api_tracks = ApiService.new().get_tracks(playlist_source_name: self.playlist_source_name)
 
-    sequence_tracks_ids = self.tracks.map(&:track_source_id)
+    # sequence_tracks_ids = self.tracks.map(&:track_source_id)
     self.tracks.find_by(id: track_id).destroy
-    while timecount < (self.duration * 60000) && self.tracks.count != Track.all.count
-      # p self.tracks.count
-      # p Track.all.count
-      # ajouter une étape avec la connexion avec l'API (ORDRE DE JOJO)
-      # recupérer une liste de tracks dans l'api
-      ########
-      ## Chercher une track au hasard dans la playlist qui diffère de celle qui ont déjà été attribuées à la séquence ##
-      ########
-      track = Track.where.not(track_source_id: sequence_tracks_ids).sample
-      track.sequence = self
-      track.save
-      self.tracks << track
-      # timecount += track.duration_track
-      timecount = self.tracks.map(&:duration_track).sum
-      p "#{timecount} : #{self.duration * 60000}"
+    playlist = RSpotify::Playlist.find(self.playlist_source_id, self.playlist_source_name)
+
+    spotify_tracks = playlist.tracks.map do |track|
+      Track.new(
+        title: track.name,
+        artist: track.artists.first.name,
+        duration_track: track.duration_ms,
+        track_source_id: track.id,
+        image_url: track.album.images.dig(0, "url"),
+        sequence: self
+      )
     end
+    sequence_tracks_ids = self.tracks.map{ |track| track.track_source_id }
+    track = spotify_tracks.reject{ |track| sequence_tracks_ids.include?(track.track_source_id)}.sample
+    track.save
+    # while timecount < (self.duration * 60000) && self.tracks.count != Track.all.count
+    #   # p self.tracks.count
+    #   # p Track.all.count
+    #   # ajouter une étape avec la connexion avec l'API (ORDRE DE JOJO)
+    #   # recupérer une liste de tracks dans l'api
+    #   ########
+    #   ## Chercher une track au hasard dans la playlist qui diffère de celle qui ont déjà été attribuées à la séquence ##
+    #   ########
+    #   track = Track.where.not(track_source_id: sequence_tracks_ids).sample
+    #   track.sequence = self
+    #   track.save
+    #   self.tracks << track
+    #   # timecount += track.duration_track
+    #   timecount = self.tracks.map(&:duration_track).sum
+    #   p "#{timecount} : #{self.duration * 60000}"
+    # end
     # on va chercher une track et on initialise le timecount
     # tant qu'il est inférieur au temps indiqué par user, il va chercher d'autres tracks
     # On shuffle tous les tracks
